8e8eba99be88a17eaad496ade9382bc84e2ea391
vandanachohan <161231376+vandanachohan@users.noreply.github.com>
2024-06-13 11:07:11 +0500

    new files

---
+++ main.js
@@ -0,0 +1,14 @@
+// function tea(suger,mailk){
+//  let cooked = suger + mailk + 3;  
+//   console.log(cooked, "kitchen")
+//   return cooked;
+// }
+//  let responce = tea(2, 3) // anmol
+//  console.log(responce, "table")
+function biryani(oil, water) {
+    var cooked = oil + water + 3;
+    console.log(cooked);
+    return cooked;
+}
+var responce = biryani(2, 3); // anmol
+console.log(responce, "table");
---
+++ main.ts
@@ -0,0 +1,17 @@
+// function tea(suger,mailk){
+//  let cooked = suger + mailk + 3;  
+
+//   console.log(cooked, "kitchen")
+//   return cooked;
+// }
+//  let responce = tea(2, 3) // anmol
+//  console.log(responce, "table")
+
+ function biryani(oil,water,){
+  let cooked = oil + water + 3
+
+  console.log(cooked)
+  return cooked;
+ }
+ let responce = biryani(2 , 3) // anmol
+ console.log(responce, "table")
